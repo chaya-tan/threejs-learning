3e0aaeeafa0e56ccaad3087cccaf4f5b7a01a249
chaya-tan <chayatan.ang@gmail.com>
2023-11-25 00:34:29 +0700

    ✨ resize width when resizing window

--- 03-first-threejs-project/src/script.js
+++ 03-first-threejs-project/src/script.js
@@ -106,3 +106,13 @@ const tick = () => {
 };
 
 tick();
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
